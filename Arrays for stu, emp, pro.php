c187b963a98f7fb347894f999761e099c9d9dc88
Pratima Mane <pratimamane05@gmail.com>
2026-03-14 17:35:27 +0530

    Add arrays for student, employee, and product

---
+++ Arrays for stu, emp, pro.php
@@ -0,0 +1,22 @@
+<?php
+$student = [
+    "name" => "Riya",
+    "roll" => 101
+];
+
+$employee = [
+    "name" => "Arun",
+    "id" => 5001
+];
+
+$product = [
+    "name" => "Laptop",
+    "price" => 45000
+];
+
+print_r($student);
+echo "<br>";
+print_r($employee);
+echo "<br>";
+print_r($product);
+?>
