8245686a2b4741f3a6946f42e3a846a2355ed997
Pratima Mane <pratimamane05@gmail.com>
2026-03-14 17:34:42 +0530

    Add PHP script to display subjects from an array

---
+++ Indexed array.php
@@ -0,0 +1,8 @@
+<?php
+$subjects = ["PHP", "Python", "Computer Networks", "Software Engineering", "Project Work"];
+
+echo "Subjects:<br>";
+for ($i = 0; $i < count($subjects); $i++) {
+    echo $subjects[$i] . "<br>";
+}
+?>
