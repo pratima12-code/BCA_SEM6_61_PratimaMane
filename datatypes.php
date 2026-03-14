e78829c9c695d51c73eeb6284ad9e5f8e6424bfc
Pratima Mane <pratimamane05@gmail.com>
2026-03-14 17:20:52 +0530

    datatypes.php

---
+++ datatypes.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Preet";      // string
+$age = 21;           // integer
+$height = 5.8;       // float
+$isStudent = true;   // boolean
+
+echo "Name: $name<br>";
+echo "Age: $age<br>";
+echo "Height: $height<br>";
+echo "Student: " . ($isStudent ? "Yes" : "No");
+?>
